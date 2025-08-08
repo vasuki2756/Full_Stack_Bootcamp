2c72cd1fa3f0a865cc52fb1eea837ccf62fa963e
Vasuki G <153893690+vasuki2756@users.noreply.github.com>
2025-08-08 17:38:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import TimeCapsuleApp from './components/TimeCapsuleApp';
 
 const App = () => {
@@ -6,13 +5,4 @@ const App = () => {
 };
 
 export default App;
-=======
 
-import './App.css'
-
-function App() {
-  <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
-}
-
-export default App
->>>>>>> 8f72e35532a5a676c0c428f9c1810b04ea36fcf0
